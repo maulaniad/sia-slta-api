7ac43afc421601131701453be2012edf8f837c2c
Mameng57 <takiya2888157@gmail.com>
2023-07-11 07:30:03 +0700

    new commit, added moment and use esm

---
+++ app.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+import siswaRouter from "./server/routes/siswa.routes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({status: 200, message: "Success"});
+});
+
+app.use("/siswa", siswaRouter)
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Express runs at http://127.0.0.1:${process.env.SERVER_PORT}`)
+});
---
+++ server/config/db.js
@@ -0,0 +1,23 @@
+import mysql from "mysql2";
+import "dotenv/config";
+
+const database = mysql.createConnection(
+  {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  }
+);
+
+database.connect(
+  (error) => {
+    if (error) {
+      throw error;
+    }
+
+    console.log("DB Connection Success!");
+  }
+)
+
+export default database;
