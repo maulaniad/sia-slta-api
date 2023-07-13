ea36acc0371a5860468525d48b8dc7c8ef5e45df
Mameng57 <takiya2888157@gmail.com>
2023-07-13 21:54:01 +0700

    add update route

--- server/routes/siswa.routes.js
+++ server/routes/siswa.routes.js
@@ -1,10 +1,11 @@
 import Router from "express";
-import { create, findAll, findById } from "../controllers/siswa.controller.js";
+import { create, findAll, findById, update } from "../controllers/siswa.controller.js";
 
 const siswaRouter = Router();
 
 siswaRouter.get("/", findAll);
 siswaRouter.post("/", create);
 siswaRouter.get("/:id", findById);
+siswaRouter.put("/:id", update);
 
 export default siswaRouter;
