35bf67fc3fb769d2c9e743b5c32af694d57a273a
Mameng57 <takiya2888157@gmail.com>
2023-07-14 09:48:43 +0700

    add login route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import siswaRouter from "./server/routes/siswa.routes.js";
+import authRouter from "./server/routes/auth.routes.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ status: 200, message: "Success" });
 });
 
+app.use("/login-sek", authRouter);
 app.use("/siswa", siswaRouter);
 
 app.listen(process.env.SERVER_PORT, () => {
