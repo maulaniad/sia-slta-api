98af2cb65103ad43f3df3f93f608b9d5203ae6ba
Mameng57 <takiya2888157@gmail.com>
2023-07-14 09:49:42 +0700

    siswa calls not uses auth

--- server/routes/siswa.routes.js
+++ server/routes/siswa.routes.js
@@ -1,11 +1,13 @@
 import Router from "express";
 import { create, findAll, findById, update } from "../controllers/siswa.controller.js";
+import auth from "../middleware/auth.js";
+import { admin, siswa } from "../middleware/role.js";
 
 const siswaRouter = Router();
 
-siswaRouter.get("/", findAll);
-siswaRouter.post("/", create);
-siswaRouter.get("/:id", findById);
-siswaRouter.put("/:id", update);
+siswaRouter.get("/", [auth, siswa], findAll);
+siswaRouter.post("/", [auth, admin], create);
+siswaRouter.get("/:id", [auth, siswa], findById);
+siswaRouter.put("/:id", [auth, siswa], update);
 
 export default siswaRouter;
