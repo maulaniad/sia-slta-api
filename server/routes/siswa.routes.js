e4cfdd589e9979471c3b1b79ce920d6c84bdc572
Mameng57 <takiya2888157@gmail.com>
2023-07-16 19:48:46 +0700

    add delete route

--- server/routes/siswa.routes.js
+++ server/routes/siswa.routes.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import { create, findAll, findById, update } from "../controllers/siswa.controller.js";
+import { create, findAll, findById, update, deleteById } from "../controllers/siswa.controller.js";
 import auth from "../middleware/auth.js";
 import { admin, siswa } from "../middleware/role.js";
 
@@ -9,5 +9,6 @@ siswaRouter.get("/", [auth, siswa], findAll);
 siswaRouter.post("/", [auth, admin], create);
 siswaRouter.get("/:id", [auth, siswa], findById);
 siswaRouter.put("/:id", [auth, siswa], update);
+siswaRouter.delete("/:id", [auth, admin], deleteById)
 
 export default siswaRouter;
