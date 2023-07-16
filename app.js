690f37ab602a049d933fb8506dac9c0d4fb658b8
Mameng57 <takiya2888157@gmail.com>
2023-07-16 19:47:51 +0700

    add exclamation mark to every success requests

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.status(200).json({ status: 200, message: "Success" });
+  res.status(200).json({ status: 200, message: "Success!" });
 });
 
 app.use("/login-sek", authRouter);
