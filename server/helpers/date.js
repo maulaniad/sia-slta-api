fdc2e60a5908d457e86db09574111fa3f7416ca6
Mameng57 <takiya2888157@gmail.com>
2023-07-17 10:25:33 +0700

    add helper function and enums to parse date

---
+++ server/helpers/date.js
@@ -0,0 +1,22 @@
+import moment from "moment";
+
+class DateFormat {
+  constructor(format) {
+    this.value = format
+  }
+
+  static FromMysql = new DateFormat("DD-MM-YYYY");
+  static ToMysql = new DateFormat("YYYY-MM-DD");
+
+  toString = () => {
+    return `${this.value}`;
+  }
+}
+
+const convertDate = (mysqlDate, format) => {
+  const convertedDate = moment.utc(mysqlDate).toDate();
+
+  return moment(convertedDate).format(format);
+}
+
+export { DateFormat, convertDate }
