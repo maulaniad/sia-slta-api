bdf590dea0b1c889317e1677f05da5a8b111951d
Mameng57 <takiya2888157@gmail.com>
2023-07-18 12:19:20 +0700

    fix the ordering of conflicting routes in siswa

--- server/routes/siswa.routes.js
+++ server/routes/siswa.routes.js
@@ -5,11 +5,11 @@ import { admin, siswa } from "../middleware/role.js";
 
 const siswaRouter = Router();
 
+siswaRouter.get("/:id", [auth, siswa], findById);
+siswaRouter.get("/", [auth, siswa], findAll);
+siswaRouter.put("/update", [auth, siswa], update);
 siswaRouter.post("/", [auth, admin], create);
 siswaRouter.put("/:id", [auth, admin], update);
 siswaRouter.delete("/:id", [auth, admin], deleteById);
-siswaRouter.get("/", [auth, siswa], findAll);
-siswaRouter.get("/:id", [auth, siswa], findById);
-siswaRouter.put("/update", [auth, siswa], update);
 
 export default siswaRouter;
