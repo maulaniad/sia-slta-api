4f216b757c07342fd5e22ece8680c337ef93f1ef
Mameng57 <takiya2888157@gmail.com>
2023-07-24 08:55:16 +0700

    forgot about the auth for admin, now it's added

--- server/routes/absensi.routes.js
+++ server/routes/absensi.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { create, findAll, update } from "../controllers/absensi.controller.js";
+import auth from "../middleware/auth.js";
+import { admin } from "../middleware/role.js";
 
 const absensiRouter = Router();
 
-absensiRouter.post("/", create);
-absensiRouter.get("/:id", findAll);
-absensiRouter.put("/:id", update);
+absensiRouter.post("/", [auth, admin], create);
+absensiRouter.get("/:id", [auth, admin], findAll);
+absensiRouter.put("/:id", [auth, admin], update);
 
 export default absensiRouter;
